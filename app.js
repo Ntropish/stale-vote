8e015cb4f7c77036817194689b03b17aff96da34
NTrope <ju5t1n5t0n3@gmail.com>
2015-08-02 17:44:13 -0500

    Removed favicon require from app.js because it was failing and crashing Heroku
    It was never used for this project

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /* global console: false, $: false, require: false, __dirname: false, process: false, module: true */
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
